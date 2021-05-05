57d250f6fd8bbb738719a0ac1ec91ebfaf13021a
hirokuni-k <shisenramen@yahoo.co.jp>
2021-05-05 22:43:10 +0900

    destroyアクション追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,12 +13,18 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(parms[:id])
+    @task = Task.find(params[:id])
   end
 
   def update
     @task = Task.find(params[:id])
     @task.update(task_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
     redirect_to tasks_path
   end
 
